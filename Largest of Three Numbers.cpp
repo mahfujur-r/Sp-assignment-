50eb09129ac3c3ec973ceaed165fc6d2017fc56d
mahfujur-r <rahmanmahfujur0701@gmail.com>
2025-02-23 20:15:08 +0600

    Update Largest of Three Numbers.cpp

--- Largest of Three Numbers.cpp
+++ Largest of Three Numbers.cpp
@@ -2,23 +2,17 @@
 
 int main()
 {
-
     double a, b,c;
-
     scanf("%lf %lf %lf", &a, &b, &c);
-
     if (a>= b && a>= c)
     {
         printf("%.2f largest", a);
     }
-
     if (b>= a && b>=c)
     {
         printf("%.2f largest",b);
 
     }
-
-
     if (c >= a &&c >= b)
     {
         printf("%.2f largest", c);
